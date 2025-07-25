0917461ccc05a869f0623abe1c29e9b3cafbd124
Donald-Edianm <kobbyedinamdonald@gmail.com>
2025-07-25 08:29:45 +0000

    Add: Included production URL

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 # Configure CORS to allow requests from the frontend
-CORS(app, origins=['http://localhost:5173', 'https://agricom-fic3.onrender.com/#/'])
+CORS(app, origins=['http://localhost:5173', 'https://agricom-fic3.onrender.com/'])
 
 UPLOAD_FOLDER = "uploads"
 Path(UPLOAD_FOLDER).mkdir(exist_ok=True)
